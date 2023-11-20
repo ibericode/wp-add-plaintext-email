ceeeac7884a74c2b492588810ee68ac73ae1b667
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-11-20 10:56:38 +0100

    updated autoloader

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dannyvankooten/wp-add-plaintext-email',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '59f12f58e11dacff611b63e2d0d5a0dabfd1567c',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v2.2.0',
+            'version' => '2.2.0.0',
+            'reference' => 'c29dc4b93137acb82734f672c37e029dfbd95b35',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'dannyvankooten/wp-add-plaintext-email' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '59f12f58e11dacff611b63e2d0d5a0dabfd1567c',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
